0baf53d569593adab2780e1c90f6d72df5a32b49
ludo514 <ludovict40@gmail.com>
2023-01-24 23:52:49 +0100

    listen port

--- app.js
+++ app.js
@@ -132,6 +132,12 @@ app.get("/:id", function(req, res) {
     })
 })
 
-app.listen(3000, function(){
+let port = process.env.PORT
+
+if(port == null || port == ""){
+    port = 3000
+}
+
+app.listen(port, function(){
     console.log("Server is runing")
 })
